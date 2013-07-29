5b3dac3e539dddecc84a97df4c205a764b556bfe
John Kearney <john.kearney@swrve.com>
2013-07-29 12:03:36 -0700

    Remove unused debug_url

--- lib/swrve/configuration.rb
+++ lib/swrve/configuration.rb
@@ -2,14 +2,13 @@ require 'faraday'
 
 module Swrve
   class Configuration
-    attr_accessor :ab_test_url, :api_url, :web_app_version, :api_key, :local_resource_path, :game_id, :debug_url,
+    attr_accessor :ab_test_url, :api_url, :web_app_version, :api_key, :local_resource_path, :game_id,
                   :load_local_resources, :debug, :http_adapter, :api_version, :resource_timeout
 
     def initialize
       @ab_test_url = 'https://abtest.swrve.com'
       @api_url = 'https://api.swrve.com'
       @web_app_version = '1.0'
-      @debug_url = 'https://debug.api.swrve.com'
       @http_adapter = Faraday.default_adapter
       @api_version  = 1
       @resource_timeout = 4
