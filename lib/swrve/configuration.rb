9043b7967aa931cf96a8b6d55a3f998bee136e01
John O'Gara <johnogara@gmail.com>
2013-07-31 08:37:28 +0100

    Revert "Add game specific default URLs for endpoints"
    This reverts commit cbdb2e22bcc087151e6bd9a0e611822b266adabc.

--- lib/swrve/configuration.rb
+++ lib/swrve/configuration.rb
@@ -2,30 +2,16 @@ require 'faraday'
 
 module Swrve
   class Configuration
-    attr_accessor :web_app_version, :api_key, :local_resource_path, :game_id,
-                  :load_local_resources, :http_adapter, :api_version, :resource_timeout
+    attr_accessor :ab_test_url, :api_url, :web_app_version, :api_key, :local_resource_path, :game_id,
+                  :load_local_resources, :debug, :http_adapter, :api_version, :resource_timeout
 
     def initialize
+      @ab_test_url = 'https://abtest.swrve.com'
+      @api_url = 'https://api.swrve.com'
       @web_app_version = '1.0'
       @http_adapter = Faraday.default_adapter
       @api_version  = 1
       @resource_timeout = 4
-    end
-
-    def ab_test_url
-      raise 'Must specify game_id for Swrve Configuration' if game_id.nil?
-      @ab_test_url || "https://#{game_id}.abtest.swrve.com"
-    end
-    def ab_test_url=(url)
-      @ab_test_url = url
-    end
-
-    def api_url
-      raise 'Must specify game_id for Swrve Configuration' if game_id.nil?
-      @api_url || "https://#{game_id}.api.swrve.com"
-    end
-    def api_url=(url)
-      @api_url = url
     end
   end
 end
